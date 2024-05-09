cd5b411d4d188c22b57a4eac67a0b7fbb395f7c3
Zahrotul <zahrotuljannah693@gmail.com>
2024-05-09 15:54:30 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function showAlert(){
+    function tampil(){
+        alert("Rasakan kelezatan yang sesungguhnya dari setiap gigitan Pempek Palembang, sebuah warisan kuliner yang tak tertandingi.")
+    }
+    tampil(); // Memanggil fungsi tampil()
+}
